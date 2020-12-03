7801c0a87136fef357063ccd47461a6a67556482
Whitaker Trebella <wtrebella@gmail.com>
2020-12-03 16:27:10 -0600

    - day 3, god this shouldn't have been so tricky

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <unistd.h>
 
 using namespace std;
 
@@ -28,8 +27,14 @@ bool isValidForPuz2(PasswordEntry entry);
 // day 3
 const int DAY3_COURSE_WIDTH = 31;
 
-vector<vector<char>> getTobogganCourse(string path);
+typedef vector<vector<char>> Course;
 
+Course getCourseTemplate(string path);
+Course courseTemplate;
+Course course;
+void expandCourseIfNeeded(int horizontalIndex, Course& course, const Course& courseTemplate);
+void printCourse(const Course &course);
+void traverseCourse(int startX, int startY, int ruleX, int ruleY, int &numTrees, Course& course, const Course& courseTemplate);
 
 int main()
 {
@@ -65,37 +70,116 @@ int main()
     cout << "Day 2 Puzzle 2: number of valid passwords = " << puz2validEntries.size() << endl;
 
     // Day 3 Puzzle 1
-    vector<vector<char>> course = getTobogganCourse("data/day3.txt");
+    courseTemplate = getCourseTemplate("data/day3.txt");
+    expandCourseIfNeeded(DAY3_COURSE_WIDTH * 4, course, courseTemplate);
+
+    int numTreesPuz1 = 0;
+    traverseCourse(0, 0, 3, 1, numTreesPuz1, course, courseTemplate);
+
+    cout << "Day 3 Puzzle 1: number of trees = " << numTreesPuz1 << endl;
+
+    int numTrees1 = 0;
+    traverseCourse(0, 0, 1, 1, numTrees1, course, courseTemplate);
+
+    int numTrees2 = 0;
+    traverseCourse(0, 0, 3, 1, numTrees2, course, courseTemplate);
+
+    int numTrees3 = 0;
+    traverseCourse(0, 0, 5, 1, numTrees3, course, courseTemplate);
+
+    int numTrees4 = 0;
+    traverseCourse(0, 0, 7, 1, numTrees4, course, courseTemplate);
+
+    int numTrees5 = 0;
+    traverseCourse(0, 0, 1, 2, numTrees5, course, courseTemplate);
+
+    long long numTreesMultPuz2 = (long long)numTrees1 * numTrees2 * numTrees3 * numTrees4 * numTrees5;
+
+    cout << "Day 3 Puzzle 2: " << numTrees1 << " * " << numTrees2 << " * " << numTrees3 << " * " << numTrees4 << " * " << numTrees5 << " = " << numTreesMultPuz2 << endl;
     
     return 0;
 }
 
-vector<vector<char>> getTobogganCourse(string path)
+void traverseCourse(int startX, int startY, int ruleX, int ruleY, int &numTrees, Course& course, const Course &courseTemplate)
+{
+    if (ruleX == 0 || ruleY == 0)
+    {
+        cout << "x and y rules must be over 0" << endl;
+    }
+
+    int height = course.size();
+
+    expandCourseIfNeeded(startX, course, courseTemplate);
+    if (course[startY][startX] == '#')
+    {
+        numTrees++;
+    }
+
+    int newX = startX + ruleX;
+    int newY = startY + ruleY;
+
+    if (newY >= height)
+    {
+        return;
+    }
+    else
+    {
+        traverseCourse(newX, newY, ruleX, ruleY, numTrees, course, courseTemplate);
+    }
+}
+
+void printCourse(const Course& course)
+{
+    for (int i = 0; i < course.size(); i++)
+    {
+        string line(course[i].begin(), course[i].end());
+        cout << line << endl;
+    }
+}
+
+void expandCourseIfNeeded(int horizontalIndex, Course &course, const Course& courseTemplate)
+{
+    if (course.size() == 0)
+    {
+        course = courseTemplate;
+    }
+
+    int targetWidth = DAY3_COURSE_WIDTH * (horizontalIndex / DAY3_COURSE_WIDTH + 1);
+    
+    while (course[0].size() < targetWidth)
+    {
+        for (int i = 0; i < course.size(); i++)
+        {
+            const vector<char> lineTemplate = courseTemplate[i];
+            course[i].insert(course[i].end(), lineTemplate.begin(), lineTemplate.end());
+        }
+    }
+}
+
+Course getCourseTemplate(string path)
 {
     string line;
     ifstream file(path);
-    vector<vector<char>> course;
+    Course courseTemplate;
     if (file.is_open())
     {
         while (getline(file, line))
         {
             vector<char> lineVect(line.begin(), line.end());
-            course.push_back(lineVect);
-            
-            for (int i = 0; i < lineVect.size(); i++)
-            {
-                char c = lineVect[i];
-                cout << c;
-                
-                if (i == DAY3_COURSE_WIDTH - 1)
-                {
-                    cout << endl;
-                }
-            }
+            courseTemplate.push_back(lineVect);
         }
     }
 
-    return course;
+    return courseTemplate;
+}
+
+void printCourse(Course course)
+{
+    for (int i = 0; i < course.size(); i++)
+    {
+        string line(course[i].begin(), course[i].end());
+        cout << line << endl;
+    }
 }
 
 void removeInvalidEntries(vector<PasswordEntry> &entries, bool validation (PasswordEntry))
